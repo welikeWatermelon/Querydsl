8256a00aee46d7c36bc4e19a6c3e25255e1e8b03
admin <bill5500@naver.com>
2025-07-14 23:25:52 +0900

    초기 커밋: QueryDSL 프로젝트 업로드

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,22 @@
+package study.querydsl;
+
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Team;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before(){
+        Team
+    }
+}
